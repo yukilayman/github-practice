a96aacccd6e545a2fbc7f91a1f840b1359a70513
yukilayman <yukilayman@gmail.com>
2023-08-01 11:08:34 -0400

    Initial Commit

---
+++ github practice/github_practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  github_practiceApp.swift
+//  github practice
+//
+//  Created by scholar on 8/1/23.
+//
+
+import SwiftUI
+
+@main
+struct github_practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
